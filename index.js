73a9dd4b7fe3b985f904336f12176d3f7f74f243
JitterBonsai <113401234+JitterBonsai@users.noreply.github.com>
2023-07-03 18:20:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+/* IMPORTACAO DO PACOTE EXPRESS */
+const express = require('express');
+
+/*INSTANCIA EXECUTAVEL DO EXPRESS*/
+const app = express();
+
+/*HABILITA A APLICAÇÃO A MANIPULAR JSON*/
+app.use(express.json());
+
+/*HABILITA A APLICAÇÃO A MANIPULA DADOS DE UM CORPO DE DADOS*/
+app.use(express.urlencoded({extended:true}))
+
+/*CRIAÇÃO DAS ROTAS */
+const filme = require('./controller/filme');
+app.use('/', filme);
+
+/*CRIAÇÃO */
+app.listen(3000, ()=>{
+    console.log('Servidor rodando em http://localhost:3000/');
+});
